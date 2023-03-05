5217370f83229af59eeca3124a12629f37c84a23
TMFfa <81461670+TMFfa@users.noreply.github.com>
2023-03-05 09:22:07 +0800

    需要修改的config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# 教务处登录的账号密码
+username = '2022*****'  # 学号
+password = '**************'  # 密码
+
+# email config
+config = {
+    'from': 'send_from@example.com',  # 邮件发送方，需要开通SMTP服务
+    'pwd': 'abcdefghyukj',  # 开通SMTP后给的登录秘钥
+    'to': ['send_to@example.com']  # 邮件接收方，可以是自己，也可以多人
+}
