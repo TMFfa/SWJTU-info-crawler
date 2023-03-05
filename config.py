a38ebe2a01f15e240079a7916ea3966324234f75
TMFfa <81461670+TMFfa@users.noreply.github.com>
2023-03-05 09:21:29 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-username = '2022112275'
-password = 'tangmaofa0801'
-
-# email config
-config = {
-    'from': 'f.cage.f@foxmail.com',
-    'pwd': 'iflxoqisbtgidfhd',
-    'to': ['3076808977@qq.com']
-}
